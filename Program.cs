9f94bade009b708d2a997b47edd3f28d2740ac2b
wuyeelok <wuyeelok@gmail.com>
2025-02-17 14:20:57 +0800

    String replace method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! Part 6");
+
+string name = "Tom";
+string name2 = "Cherry";
+
+string friends = $"Hello, {name} and {name2}";
+Console.WriteLine(friends);
+
+Console.WriteLine(friends.Replace("Tom", "Susan"));
+
